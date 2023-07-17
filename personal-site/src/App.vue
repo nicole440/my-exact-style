b663bd31c9bddfb34528f8480e4b368d4a8a4a8a
Nicole <111709554+nicole440@users.noreply.github.com>
2023-07-17 17:22:08 -0400

    Adjust background image positioning and width of main text

--- personal-site/src/App.vue
+++ personal-site/src/App.vue
@@ -38,11 +38,12 @@
 body {
   background-image: url(https://images.unsplash.com/uploads/141155339325423394b24/03982423?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80);
   background-size: auto;
+  background-position: fixed;
 }
 
 .intro-text > div {
   margin-bottom: 30px;
-  max-width: 50%;
+  max-width: 40%;
 }
 h1 {
   font-weight: 400;
