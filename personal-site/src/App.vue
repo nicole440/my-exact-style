20ea08037c2480cc014b410c5d34286039d1427e
Nicole <111709554+nicole440@users.noreply.github.com>
2023-07-18 15:59:24 -0400

    Revise text

--- personal-site/src/App.vue
+++ personal-site/src/App.vue
@@ -14,7 +14,7 @@
       <p class="detail">I have a passion for harnessing the power of data using Java and Python. Whether it's building algorithms or developing efficient data processing systems, I find joy in crafting elegant solutions that drive insightful outcomes.</p>
     </div>
     <div>
-      <p class="detail">From designing solid database architectures to writing efficient queries and ensuring seamless integration with applications, I also love working with databases, diving deep into the intricacies of data storage, retrieval, and optimization.</p>
+      <p class="detail">I also love working with databases, from designing solid database architectures to writing efficient queries and ensuring seamless integration with applications. I enjoy diving deep into the intricacies of data storage, retrieval, and optimization.</p>
     </div>
     <div>
       <p class="detail">When I'm not programming, I enjoy birdwatching, Brazilian jiu jitsu, and spoiling my dog and three cats.
