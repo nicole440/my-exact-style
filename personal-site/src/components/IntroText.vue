f60088f756a90908e8152ec4d7c2c84ae9d9b642
Nicole <111709554+nicole440@users.noreply.github.com>
2023-07-20 16:26:26 -0400

    Move text to Home view and un-scope styles on component.

--- personal-site/src/components/IntroText.vue
+++ personal-site/src/components/IntroText.vue
@@ -2,32 +2,10 @@
 </script>
 
 <template>
-    <div class="intro-text">
-        <div class="first">
-            <h1>Hi, I'm Nicole.</h1>
-            <p>I'm a software developer in Lancaster County, PA.</p>
-        </div>
-        <div>
-            <p class="detail">I have a passion for harnessing the power of data using Java and Python. Whether it's building
-                algorithms or developing efficient data processing systems, I find joy in crafting elegant solutions that
-                drive
-                insightful outcomes.</p>
-        </div>
-        <div>
-            <p class="detail">I also love working with databases, from designing solid database architectures to writing
-                efficient queries and ensuring seamless integration with applications. I enjoy diving deep into the
-                intricacies of
-                data storage, retrieval, and optimization.</p>
-        </div>
-        <div>
-            <p class="detail">When I'm not programming, I enjoy birdwatching, Brazilian jiu jitsu, and spoiling my rescue
-                animals with love.
-            </p>
-        </div>
-    </div>
+    
 </template>
 
-<style scoped>
+<style>
 .intro-text {
     padding-left: 10px;
 }
