4bb87dce388cc2d57d072eaa543ac81ab307389e
Nicole <111709554+nicole440@users.noreply.github.com>
2023-07-21 13:02:13 -0400

    Minor edits for SEO

--- personal-site/src/App.vue
+++ personal-site/src/App.vue
@@ -4,7 +4,7 @@
 <template>
   <div>
     <nav>
-      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/nicole-groff">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
       <RouterLink to="/projects">Projects</RouterLink>
       <RouterLink to="/contact">Contact</RouterLink>
