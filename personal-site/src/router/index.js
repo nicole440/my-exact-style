7a8e9618b67086afa13cffcd9ecd8c4828a52369
Nicole <111709554+nicole440@users.noreply.github.com>
2023-07-21 13:01:23 -0400

    Minor edits for SEO

--- personal-site/src/router/index.js
+++ personal-site/src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/nicole-groff',
       name: 'home',
       component: HomeView
     },
