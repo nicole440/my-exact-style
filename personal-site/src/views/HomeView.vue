f6fe285327c01375b9685bd9a04ecfe62c380bdf
Nicole <111709554+nicole440@users.noreply.github.com>
2023-07-24 15:05:19 -0400

    Rearrange personal info, add photo, and revise text content.

--- personal-site/src/views/HomeView.vue
+++ personal-site/src/views/HomeView.vue
@@ -18,45 +18,34 @@ export default {
       <div>
         <h1>Hi, I'm Nicole.</h1>
         <p>I'm a software developer in Lancaster County, PA.</p>
-      </div>
-      <div>
-
-        <div>
-          <p class="detail">Before I fell in love with programming, I worked as a paralegal, navigating the worlds of
-            business, commercial real estate, and liquor law. One of the most rewarding parts of my role was working
-            behind the scenes to support small business owners in making their dreams a reality.</p>
+        <div class="photo-container">
+          <img class="photo" src="src/assets/IMG_5690~5.JPG" alt="Photo of Nicole Groff">
         </div>
-      </div>
-      <div>
-        <h2>As a developer, my mission remains the same: to continue creating a positive impact for others.
-        </h2>
-      </div>
-      <div>
-        <p class="detail">I've developed a passion for working with data using Java and Python. Building algorithms and
-          crafting efficient data processing systems brings me joy, as I strive for elegant solutions that drive
-          insightful outcomes.</p>
-      </div>
-      <div>
-        <p class="detail">I also love working with databases, from designing solid database
-          architectures to optimizing queries and ensuring seamless application integration. I enjoy diving deep into the
-          intricacies of data storage and retrieval.</p>
-      </div>
-      <div>
-        <p class="detail">Outside of programming, I find solace in birdwatching, showering my rescue animals with love,
-          and continually honing my skills and
-          discipline in Brazilian jiu-jitsu.</p>
-      </div>
-      <div>
-        <p>I'm currently seeking opportunities in backend development or data engineering.</p>
-      </div>
-      <div>
         <p class="detail">
-          I'm eager to contribute my unique perspective, passion for programming, and enthusiasm for problem-solving as
-          part of a cohesive team.
+          <ul>
+            <li>I'm detail-oriented, yet context-driven.</li>
+            <li>A pragmatic thinker with a positive attitude.</li>
+            <li>I'm self-motived, naturally curious, and always striving to enhance my skills and take on new challenges.</li>
+          </ul>
         </p>
       </div>
     </div>
   </div>
 </template>
 
-<style></style>
+<style>
+.photo-container {
+  /* background-color: rgba(224, 216, 190, 0.61); */
+  margin: 10px auto;
+  max-width: fit-content;
+  /* border-radius: 10px; */
+}
+.photo {
+  border-radius: 50%;
+  height: 175px;
+}
+
+li {
+  list-style:circle;
+}
+</style>
