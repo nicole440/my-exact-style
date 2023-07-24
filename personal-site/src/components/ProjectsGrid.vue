3c3325f780409b5030962a16cb8a5d8d30628ce3
Nicole <111709554+nicole440@users.noreply.github.com>
2023-07-24 15:03:26 -0400

    Add responsiveness and adjustments to design

--- personal-site/src/components/ProjectsGrid.vue
+++ personal-site/src/components/ProjectsGrid.vue
@@ -1,66 +1,8 @@
 <script></script>
 
-<template>
-    <div class="projects-container">
-        <div class="project">
-            <div class="project-info">
-                <div class="project-header">
-                    <h1>Recording Fee Calculator</h1>
-                    <a class="github-link" href="https://github.com/nicole440/bird-observer" target="_blank"><img
-                            class="github-logo" src="../assets/GitHub-PNG-Isolated-Photo.png" alt="GitHub logo"></a>
-                </div>
-                <div class="project-description">
-                    <p class="description">Where it all began! I started this project when I was learning JavaScript in 2022
-                        as I found myself considering the ways in which I could incorporate my beginner-level programming
-                        knowledge into my work as a real estate paralegal. It began as a simple conditional statement in
-                        vanilla JS, and I have expanded on its functionality and given it a simple UI with Vue.js.</p>
-                </div>
-            </div>
-            <div class="project-screenshot">
-                <p>(Image coming soon.)</p>
-            </div>
-        </div>
-        <div class="project">
-            <div class="project-info">
-                <div class="project-header">
-                    <h1>Crime Data Analysis</h1>
-                    <a class="github-link" href="https://github.com/nicole440/crime-data-analysis" target="_blank"><img
-                            class="github-logo" src="../assets/GitHub-PNG-Isolated-Photo.png" alt="GitHub logo"></a>
-                </div>
-                <div class="project-description">
-                    <p class="description">This is a Python program written using Jupyter Notebook and utilizing the pandas
-                        library to explore, transform, clean, and analyze data on hate crimes in New York City between 2019
-                        and 2023, and I used Power BI to create visualizations of the data.</p>
-                </div>
-            </div>
-            <div class="project-screenshot">
-                <p>(Image coming soon.)</p>
-            </div>
-        </div>
-        <div class="project">
-            <div class="project-info">
-                <div class="project-header">
-                    <h1>eBird Data Pipeline</h1>
-                    <a class="github-link" href="https://github.com/nicole440/dryocopus-pileatus" target="_blank"><img
-                            class="github-logo" src="../assets/GitHub-PNG-Isolated-Photo.png" alt="GitHub logo"></a>
-                </div>
-                <div class="project-description">
-                    <p class="description">This is a Python program written in a Jupyter Notebook that extracts recent
-                        observation and birding hotspot data from the Cornell Lab of Ornithology eBird API. Once extracted,
-                        it transforms the data and loads it into a SQLite database. I'm currently working on configuring an
-                        email notification feature for updates on recent bird sightings based on certain criteria.</p>
-                </div>
-            </div>
-            <div class="project-screenshot">
-                <p>(Image coming soon.)</p>
-            </div>
-        </div>
-        <div class="project"></div>
-    </div>
-</template>
+<template></template>
 
-<style scoped>
-/* TODO: Make this responsive*/
+<style>
 
 .projects-container {
     display: grid;
@@ -72,16 +14,28 @@
     grid-template-columns: 1fr 1fr;
     grid-template-areas:
         "info screenshot";
-    margin-bottom: 30px;
-}
-
-.project-description, .project-header {
+    margin-bottom: 50px;
+    border: 1px solid lightgrey;
+    border-radius: 20px;
+    padding: 30px;
     background-color: #ffffffb2;
 }
 
+@media (max-width: 600px) {
+    .project {
+        display: flex;
+        flex-wrap: wrap;
+        text-align: center;
+    }
+}
+
+.project-description {
+    margin-bottom: 10px;
+}
+
+.project-description,
 .project-header {
-    display: flex;
-    align-content: flex-start;
+    background-color: #ffffffb2;
 }
 
 .project-screenshot {
@@ -89,10 +43,11 @@
     margin: auto;
     padding: 10px;
     background-color: #eceaeac7;
+    text-align: center;
 }
 
 h1 {
-    grid-area: name;
+    grid-area: info;
     font-size: 1.7rem;
     margin: 0;
 }
@@ -107,6 +62,7 @@ h1 {
 }
 
 .description {
+    grid-area: info;
     font-size: 1.2rem;
 }
 
@@ -115,8 +71,7 @@ h1 {
     padding: 10px;
 }
 
-.subtext > span {
+.subtext>span {
     font-weight: 400;
 }
-
 </style>
--- personal-site/src/views/ProjectsView.vue
+++ personal-site/src/views/ProjectsView.vue
@@ -14,11 +14,77 @@ export default {
   <div>
     <HelloWorld/>
     <ProjectsGrid/>
-    <div class="main-text">
-      <div>
-        <h1>This page is still under construction.</h1>
-        <p>Please check back later for updates.</p>
-      </div>
+
+      <div class="projects-container">
+        <div class="project">
+            <div class="project-info">
+                <div class="project-header">
+                    <h1>Recording Fee Calculator</h1>
+                    <a class="github-link" href="https://github.com/nicole440/bird-observer" target="_blank"><img
+                            class="github-logo" src="../assets/GitHub-PNG-Isolated-Photo.png" alt="GitHub logo"></a>
+                </div>
+                <div class="project-description">
+                    <p class="description">I started this project when I was learning JavaScript in 2022
+                        as I found myself considering the ways in which I could incorporate my beginner-level programming
+                        knowledge into my work as a real estate paralegal. It began as a simple conditional statement in
+                        vanilla JS, and I have expanded on its functionality and given it a simple UI with Vue.js.</p>
+                </div>
+            </div>
+            <div class="project-screenshot">
+                <p>(Image coming soon)</p>
+            </div>
+        </div>
+        <div class="project">
+            <div class="project-info">
+                <div class="project-header">
+                    <h1>Crime Data Analysis</h1>
+                    <a class="github-link" href="https://github.com/nicole440/crime-data-analysis" target="_blank"><img
+                            class="github-logo" src="../assets/GitHub-PNG-Isolated-Photo.png" alt="GitHub logo"></a>
+                </div>
+                <div class="project-description">
+                    <p class="description">This is a Python program written using Jupyter Notebook and utilizing the pandas
+                        library to explore, transform, clean, and analyze data on hate crimes in New York City between 2019
+                        and 2023, and I used Power BI to create visualizations.</p>
+                </div>
+            </div>
+            <div class="project-screenshot">
+                <p>(Image coming soon)</p>
+            </div>
+        </div>
+        <div class="project">
+            <div class="project-info">
+                <div class="project-header">
+                    <h1>eBird Data Pipeline</h1>
+                    <a class="github-link" href="https://github.com/nicole440/dryocopus-pileatus" target="_blank"><img
+                            class="github-logo" src="../assets/GitHub-PNG-Isolated-Photo.png" alt="GitHub logo"></a>
+                </div>
+                <div class="project-description">
+                    <p class="description">This is a Python program written in a Jupyter Notebook that extracts recent
+                        observation and birding hotspot data from the Cornell Lab of Ornithology eBird API. Once extracted,
+                        it transforms the data and loads it into a SQLite database. I'm currently working on configuring an
+                        email notification feature for updates on recent bird sightings based on specified criteria.</p>
+                </div>
+            </div>
+            <div class="project-screenshot">
+                <p>(Image coming soon)</p>
+            </div>
+        </div>
+        <div class="project">
+            <div class="project-info">
+                <div class="project-header">
+                    <h1>HopSeeker</h1>
+                    <a class="github-link" href="https://github.com/nicole440/hop-seeker" target="_blank"><img
+                            class="github-logo" src="../assets/GitHub-PNG-Isolated-Photo.png" alt="GitHub logo"></a>
+                </div>
+                <div class="project-description">
+                    <p class="description">This is a A full-stack application that consumes the OpenBrewery public API to
+                        allow users to search by name, city, or zip code to find Pennsylvania breweries. Registered users can create and maintain a list of their favorites.</p>
+                </div>
+            </div>
+            <div class="project-screenshot">
+                <p>(Image coming soon)</p>
+            </div>
+        </div>
     </div>
   </div>
 </template>
