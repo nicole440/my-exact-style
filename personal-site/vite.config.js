d71de0db2de8fdfdd9c0fbb3bc2b2bae163063c3
Nicole <111709554+nicole440@users.noreply.github.com>
2023-07-25 13:19:35 -0400

    Configure vite to handle assets

--- personal-site/vite.config.js
+++ personal-site/vite.config.js
@@ -6,4 +6,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/',
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      external: ['src/assets/linkedin.png', 'src/assets/GitHub-PNG-Isolated-Photo.png'],
+    }
+  }
 })
